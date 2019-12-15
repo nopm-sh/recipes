8483631a865a66777aa6e707c5ffce071fa1b16e
Benjamin Jorand <benjamin.jorand@botsunit.com>
2019-12-15 13:15:53 +0100

    wip

--- go.sh
+++ go.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# nopm:subst VERSION type:version
+# nopm:subst version
+# nopm:url https://golang.org
+# nopm:compat darwin x86_64
+# nopm:compat linux x86_64
 
 set -e
 
@@ -12,7 +15,7 @@ case "$arch" in
     arch="amd64"
     ;;
 "i686" | "i")
-    arch="i385"
+    arch="i386"
     ;;
 *)
     echo "Incompatible architecture $arch"
@@ -21,34 +24,35 @@ case "$arch" in
 esac
 
 version=""
+base_url=https://dl.google.com/go/go
 
 if [ -z $version ]
 then
-  base_url=https://dl.google.com/go/go
   download_page=$(mktemp /tmp/nopm.sh-recipe-go.XXXXXX)
   trap 'rm -f -- "$DOWNLOAD_PAGE"' EXIT
-  wget --quiet https://golang.org/dl/ -O $download_page
-  download_url=$(cat $download_page | grep "$base_url[[:digit:]]\.[[:digit:]]\{1,2\}\.[[:digit:]]\{1,2\}.$os.$arch\.tar\.gz" | head -n1 | sed -e "s#.*href=\"##" -e "s/\".*//")
-  version=$(echo $download_url | grep "$base_url[[:digit:]]\.[[:digit:]]\{1,2\}\.[[:digit:]]\{1,2\}.$os.$arch\." \
+  curl -s https://golang.org/dl/ > $download_page
+  download_url=$(cat $download_page | grep "$base_url[[:digit:]]\.[[:digit:]]\{1,2\}\.[[:digit:]]\{1,2\}.$os-$arch\.tar\.gz" | head -n1 | sed -e "s#.*href=\"##" -e "s/\".*//")
+  version=$(echo $download_url | grep "$base_url[[:digit:]]\.[[:digit:]]\{1,2\}\.[[:digit:]]\{1,2\}.$os-$arch\." \
   | head -n1 \
   | sed "s#.*$base_url##" \
   | sed "s/\.$os.*//")
-
+else
+  download_url="$base_url$version.$os-$arch.tar.gz"
 fi
 
-wget --continue $download_url
+curl -s $download_url > go$version.$os-$arch.tar.gz
 tar -C /usr/local -xvzf go$version.$os-$arch.tar.gz
 
 mkdir -p ~/go/{bin,pkg,src}
 
 if [ -z $GOPATH ]
 then
-  echo "export GOPATH=~/go" >> ~/.profile && source ~/.profile
+  echo "export GOPATH=\$HOME/go" >> ~/.profile && . ~/.profile
 fi
 
 if ! which go &>/dev/null
 then
-  echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >> ~/.profile && source ~/.profile
+  echo "export PATH='$PATH':/usr/local/go/bin:$GOPATH/bin" >> ~/.profile && . ~/.profile
 fi
 
 go version
